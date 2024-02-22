eff50d6561c657837ea1d933277bc038fb963fbb
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-02-22 16:06:26 -0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/products', to: 'products#index'
-
+  get '/products/:id', to: 'products#show', as: 'product'
   # Defines the root path route ("/")
   # root "articles#index"
 end
