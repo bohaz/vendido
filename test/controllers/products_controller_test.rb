5d7100ef07b75dd143ac67674b9dba335f2305bb
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-13 15:27:22 -0400

    Add tests

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -9,4 +9,11 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "render a detailed product page" do
+    get product_path(products(:ps4))
+
+    assert_response :success
+    assert_select ".title", "PS4 Fat"
+    assert_select ".description", "PS4 en buen estado"
+    assert_select ".price", "150$"
+  end
 end
