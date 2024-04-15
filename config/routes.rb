2c3552c93bec61a8fe679bd6bd9128816f639519
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-15 09:37:11 -0400

    Add edit route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/products/new', to: 'products#new', as: 'new_product'
   get '/products', to: 'products#index'
   get '/products/:id', to: 'products#show', as: 'product'
+  get '/products/:id/edit', to: 'products#edit', as: 'edit_product'
   # Defines the root path route ("/")
   # root "articles#index"
 end
