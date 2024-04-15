ac63685dfd7657fe800be3ac7450fb3aa0394fc4
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-15 19:09:24 -0400

    Add photo relation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   validates :title, :description, :price, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
+  has_one_attached :photo
 end
