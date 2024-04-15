1fedf919c556d66c065b968b9f6ab5546e214987
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-15 09:12:35 -0400

    Add a succes notice

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,7 +14,7 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
-      redirect_to @product
+      redirect_to products_path, notice: "Tu producto se ha creado correctamente"
     else
       render :new, status: :unprocessable_entity
     end
