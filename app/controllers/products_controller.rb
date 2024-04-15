3b4df660c0383a660accd611df2c70394a89c553
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-15 08:24:05 -0400

    Add create method to controllers

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,4 +10,19 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :price)
+  end
 end  
