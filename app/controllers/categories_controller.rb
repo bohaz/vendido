23329ba99a2596224bb9206e5035aaeae0d2f801
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-16 11:08:53 -0400

    Remove show method and json format

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -21,11 +21,9 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to category_url(@category), notice: "Category was successfully created." }
-        format.json { render :show, status: :created, location: @category }
+        format.html { redirect_to categories_url, notice: "Category was successfully created." }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -34,11 +32,9 @@ class CategoriesController < ApplicationController
   def update
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to category_url(@category), notice: "Category was successfully updated." }
-        format.json { render :show, status: :ok, location: @category }
+        format.html { redirect_to categories_url, notice: "Category was successfully updated." }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -49,7 +45,6 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to categories_url, notice: "Category was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
