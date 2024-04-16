2802dda85c17ce63766d291d3f0ecbc401c9b171
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-16 11:09:10 -0400

    Remove show test

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -23,11 +23,6 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to category_url(Category.last)
   end
 
-  test "should show category" do
-    get category_url(@category)
-    assert_response :success
-  end
-
   test "should get edit" do
     get edit_category_url(@category)
     assert_response :success
