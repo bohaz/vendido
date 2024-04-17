a11df6dc3075beedcd0aa33d44f148c4abedfe4b
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-17 11:54:50 -0400

    Add pgsearch to the model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
+  include PgSearch::Model
+  pg_search_scope :search_full_text, against: {
+    title: "A",
+    description: "B"
+  }
+
   has_one_attached :photo
   validates :title, :description, :price, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
