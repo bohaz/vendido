1d40e9db884c3d6808b41c433c58415bccd13121
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-17 11:55:55 -0400

    Add some tests

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -16,6 +16,38 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_select '.product', 2
   end
 
+  test "render a list of products filtered by min_price and max_price" do
+    get products_path(min_price: 160, max_price: 200)
+    
+    assert_response :success
+    assert_select '.product', 1
+    assert_select 'h2', "Nintendo Switch" 
+  end
+
+  test "search a product by query_text" do
+    get products_path(query_text: "Switch")
+    
+    assert_response :success
+    assert_select '.product', 1
+    assert_select 'h2', "Nintendo Switch" 
+  end
+
+  test "sort products by expensive prices first" do
+    get products_path(order_by: "expensive")
+    
+    assert_response :success
+    assert_select '.product', 3
+    assert_select '.products .product:first-child h2', "MacBook Pro 2015" 
+  end
+
+  test "sort products by cheapest prices first" do
+    get products_path(order_by: "cheapest")
+    
+    assert_response :success
+    assert_select '.product', 3
+    assert_select '.products .product:first-child h2', "PS4 Fat" 
+  end
+
   test "render a detailed product page" do
     get product_path(products(:ps4))
 
