d95b2287e80964207d7bbd9664bd8ee42dda7227
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-17 11:54:14 -0400

    Add conditionals for ordering by

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,28 @@
 class ProductsController < ApplicationController
   def index
     @categories = Category.all.order(name: :asc).load_async
-    @products = Product.includes(:photo_attachment).all.order(created_at: :desc).load_async
+    @products = Product.includes(:photo_attachment).all
     if params[:category_id].present?
       @products = @products.where(category_id: params[:category_id])
     end
+    if params[:min_price].present?
+      @products = @products.where("price >= ?", params[:min_price])
+    end
+    if params[:max_price].present?
+      @products = @products.where("price <= ?", params[:max_price])
+    end
+    if params[:query_text].present?
+      @products = @products.search_full_text(params[:query_text])
+    end
+   
+    order_by = {
+        "newest": "created_at DESC",
+        "expensive": "price DESC",
+        "cheapest": "price ASC"
+    }.fetch(params[:order_by]&.to_sym, "created_at DESC")
+
+    @products = @products.order(order_by).load_async
+   
   end
 
   def show
