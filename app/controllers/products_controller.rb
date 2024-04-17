df9904465e73a544ce07794fb526a21ccecffbec
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-17 17:14:42 -0400

    Small change

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,7 +18,7 @@ class ProductsController < ApplicationController
     order_by = Product::ORDER_BY.fetch(params[:order_by]&.to_sym, Product::ORDER_BY[:newest])
 
     @products = @products.order(order_by).load_async
-   
+
     @pagy, @products = pagy_countless(@products, items: 12)
   end
 
