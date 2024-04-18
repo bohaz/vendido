95f494e10dad3df66c1e1924c98f606f63e9ae3c
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-18 16:15:03 -0400

    Path error fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,6 +21,6 @@ class ApplicationController < ActionController::Base
   end
 
   def protect_pages
-    redirect_to new_sessions_path unless Current.user
+    redirect_to new_session_path, alert: t('common.not_loged_in') unless Current.user
   end
 end
