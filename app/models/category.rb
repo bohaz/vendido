526022eeda2d868da478cdd8271981e830a50f2a
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-18 11:55:33 -0400

    Update category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  has_many :products, dependent: :restrict_with_exception
 
+  has_many :products, dependent: :restrict_with_exception
 end
