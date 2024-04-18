5dfa1c28e06ac4b1dc703b44c0621eff5866c111
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-18 13:05:19 -0400

    Add user name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
-  
+
   validates :email, presence: true, uniqueness: true
-  validates :username, presence: true, uniqueness: true
-  validates :password,length: { minimum: 6 }
+  validates :username, presence: true, uniqueness: true, 
+  length: { in: 3..15 },
+  format: { 
+    with: /\A[a-zA-Z0-9]+\Z/,
+    message: :invalid
+  }
+  
+  validates :password, length: { minimum: 6 }
 end
