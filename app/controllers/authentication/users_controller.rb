db65ea02b21bb6c331c88cc1736cfb040c371ca4
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-18 15:26:20 -0400

    Update user controller

--- app/controllers/authentication/users_controller.rb
+++ app/controllers/authentication/users_controller.rb
@@ -7,6 +7,7 @@ class Authentication::UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       redirect_to products_path, notice: t('.created')
     else
       render :new, status: :unprocessable_entity
