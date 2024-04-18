4404f144d498e13eb181c60b38e4db29a99163bb
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-18 12:46:37 -0400

    Add create method and params to the user controller

--- app/controllers/authentication/users_controller.rb
+++ app/controllers/authentication/users_controller.rb
@@ -4,6 +4,18 @@ class Authentication::UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to products_path, notice: t('.created')
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
-  
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :username, :password)
+  end
 end
