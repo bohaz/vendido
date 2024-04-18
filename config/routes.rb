5e4f2fb0548ab52931426e23681953353ff320bf
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-18 12:26:25 -0400

    Add user authentication route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :categories, exept: :show
   resources :products, path: '/'
+
+  namespace :authentication do
+    resources :users, only: [:new, :create]
+  end
 end
