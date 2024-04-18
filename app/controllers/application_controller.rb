634ea1d0b0c0acdeac3eb6296130db00af8540e1
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-18 15:25:30 -0400

    Add callback before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
-  around_action :switch_locale
   include Pagy::Backend
+
+  around_action :switch_locale
+  before_action :set_current_user
+  
 
   def switch_locale(&action)
     I18n.with_locale(locale_from_header, &action)
@@ -10,5 +13,9 @@ class ApplicationController < ActionController::Base
 
   def locale_from_header
     request.env['HTTP_ACCEPT_LANGUAGE']&.scan(/^[a-z]{2}/)&.first
+  end
+
+  def set_current_user
+    Current.user = User.find_by(id: session[:user_id]) if session[:user_id]
   end
 end
