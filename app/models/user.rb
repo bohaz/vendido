8e55f70f4d160ccca4536f75a9b55252fdf67c63
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-18 12:21:09 -0400

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+  
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+  validates :password,length: { minimum: 6 }
 end
