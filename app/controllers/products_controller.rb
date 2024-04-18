b6596c2595af2e561f5467d6568c058398f195c2
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-18 11:42:14 -0400

    Add strong params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   def index
     @categories = Category.all.order(name: :asc).load_async
     
-    @pagy, @products = pagy_countless(FindProducts.new.call(params).load_async, items: 12)
+    @pagy, @products = pagy_countless(FindProducts.new.call(product_params_index).load_async, items: 12)
   end
 
   def show
@@ -45,5 +45,9 @@ class ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:title, :description, :price, :photo, :category_id)
+  end
+
+  def product_params_index
+    params.permit(:category_id, :min_price, :max_price, :query_text, :order_by)
   end
 end  
