e69e40dd36c43d34d53925cc0f4cd48591f97c5d
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-18 12:46:51 -0400

    Update route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :categories, exept: :show
   resources :products, path: '/'
 
-  namespace :authentication do
+  namespace :authentication, path: '', as: ''  do
     resources :users, only: [:new, :create]
   end
 end
