ae56a8aa6ecef294577b9c6d040470258bc0faf1
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-18 07:56:50 -0400

    Create Find_products to refactor Query Object Pattern

---
+++ app/queries/Find_products.rb
@@ -0,0 +1,24 @@
+class FindProducts
+  attr_reader :products
+  def initialize(products = initial_scope)
+    @products = products
+  end
+
+  def call(params = {})
+  scoped = products
+  scoped = filter_by_category(scoped, params[:category_id])
+  end
+
+  private
+
+  def initial_scope
+    Product.with_attached_photo
+  end
+
+  filter_by_category(scoped, category_id)
+  return scoped unless category_id
+
+  scoped.where(category_id: category_id)
+end
+
+FindProducts.new.call({min_price: 400, max_price: 1000, query_text: 'iPhone'})
