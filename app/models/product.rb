6a6b5211a0b1a6d0f8923a4cb1beabaaadad0651
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-19 08:37:27 -0400

    Asign products to current user

--- app/models/product.rb
+++ app/models/product.rb
@@ -16,4 +16,5 @@ class Product < ApplicationRecord
   validates :price, numericality: { greater_than_or_equal_to: 0 }
   
   belongs_to :category
+  belongs_to :user, default: -> { Current.user }
 end
