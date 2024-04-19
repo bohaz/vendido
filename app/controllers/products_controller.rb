2535f8909b08afa2bdb2a3ee8d3b5b558178c567
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-19 09:08:50 -0400

    Fix param error

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -50,6 +50,6 @@ class ProductsController < ApplicationController
   end
 
   def product_params_index
-    params.permit(:category_id, :min_price, :max_price, :query_text, :order_by)
+    params.permit(:category_id, :min_price, :max_price, :query_text, :order_by, :page)
   end
 end  
