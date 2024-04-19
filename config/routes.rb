412c15bcbe7939169cf593edaf59a2bbf5ba47bb
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-19 09:23:39 -0400

    Add log out route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   namespace :authentication, path: '', as: ''  do
     resources :users, only: [:new, :create]
-    resources :sessions, only: [:new, :create]
+    resources :sessions, only: [:new, :create, :destroy]
   end
 end
