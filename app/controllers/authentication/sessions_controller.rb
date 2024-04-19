fce311aed6a0d6d762a29408a0afc268fb0a86d1
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-19 09:23:52 -0400

    Add destroy method

--- app/controllers/authentication/sessions_controller.rb
+++ app/controllers/authentication/sessions_controller.rb
@@ -16,4 +16,9 @@ class Authentication::SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    session.delete(:user_id)
+    redirect_to products_path, notice: t('.destroyed')
+  end
+
   end
