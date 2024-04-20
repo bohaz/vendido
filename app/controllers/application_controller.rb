57d4c43e46d2a9167ed6e639941e00cb6175b848
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-20 14:42:21 -0400

    Add authorize method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,8 @@ class ApplicationController < ActionController::Base
   def protect_pages
     redirect_to new_session_path, alert: t('common.not_loged_in') unless Current.user
   end
+
+  def authorize! product
+    is_allowed = product.user_id == Current.user.id
+    redirect_to products_path, alert: t('common.not_authorized') unless is_allowed
 end
