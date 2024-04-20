8576aa9c9c1bf64d68a091f5c5971a2b922d6572
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-20 15:02:20 -0400

    Add authorize method to update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -30,6 +30,7 @@ class ProductsController < ApplicationController
   end
 
   def update
+    authorize! @product
     @product = Product.find(params[:id])
     if @product.update(product_params)
       redirect_to products_path, notice: t('.updated')
