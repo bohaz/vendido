ca80150146200cdd0bd02a0c889c3d526511bff9
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-20 16:45:37 -0400

    Add admin to the table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
 
+  class NotAuthorizedError < StandardError; end
+  
+  rescue_from NotAuthorizedError do
+    redirect_to products_path, alert: t('common.not_authorized')
+  end 
+      
   around_action :switch_locale
   before_action :set_current_user
   before_action :protect_pages
-  
 
   def switch_locale(&action)
     I18n.with_locale(locale_from_header, &action)
@@ -26,6 +31,6 @@ class ApplicationController < ActionController::Base
 
   def authorize! product
     is_allowed = product.user_id == Current.user.id
-    redirect_to products_path, alert: t('common.not_authorized') unless is_allowed
+    raise NotAuthorizedError unless is_allowed
   end
 end
