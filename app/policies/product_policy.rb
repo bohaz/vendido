5ad388ad41375a6e40fc586494290a73e06ef8ae
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-20 17:51:49 -0400

    Refactor product policy code

--- app/policies/product_policy.rb
+++ app/policies/product_policy.rb
@@ -1,13 +1,13 @@
 class ProductPolicy < BasePolicy
   def edit
-    record.user_id == Current.user.id
+    record.owner?
   end
 
   def update
-    record.user_id == Current.user.id
+    record.owner?
   end
   
   def destroy
-    record.user_id == Current.user.id
+    record.owner?
   end
 end
