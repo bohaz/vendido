7fcdf4e661cfc96e7227bf30534732d9a0c6fe1f
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-20 17:28:22 -0400

    Refactor authorize method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,11 +30,8 @@ class ApplicationController < ActionController::Base
   end
 
   def authorize! record = nil
-    is_allowed =  if record
-      record.user_id == Current.user.id
-    else
-      Current.user.admin?
-    end
+    is_allowed = "#{controller_name.singularize}_policy".classify.constantize.new(record).send(action_name)
+
     raise NotAuthorizedError unless is_allowed
   end
 end
