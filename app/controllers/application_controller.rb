a9ae1b7e8f85f9075e80b289cffead3f17d9b317
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-20 15:00:00 -0400

    Fix sintax error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,4 +27,5 @@ class ApplicationController < ActionController::Base
   def authorize! product
     is_allowed = product.user_id == Current.user.id
     redirect_to products_path, alert: t('common.not_authorized') unless is_allowed
+  end
 end
