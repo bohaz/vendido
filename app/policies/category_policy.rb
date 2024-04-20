ef43e5b8b8a52cbe91a54f4b10f9127fa0b6474c
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-20 17:56:26 -0400

    Refactor category policy

--- app/policies/category_policy.rb
+++ app/policies/category_policy.rb
@@ -1,25 +1,5 @@
 class CategoryPolicy < BasePolicy
-  def index
-    Current.user.admin?
-  end
-
-  def new
-    Current.user.admin?
-  end
-
-  def create
-    Current.user.admin?
-  end
-
-  def edit
-    Current.user.admin?
-  end
-
-  def update
-    Current.user.admin?
-  end
-
-  def destroy
+  def method_missing (m, *args, &block)
     Current.user.admin?
   end
 end
