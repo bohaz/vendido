88451cb4e92aa9b09adc1a80502e8b40cf58de1d
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-22 09:38:55 -0400

    Add users route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     resources :sessions, only: [:new, :create, :destroy], path: '/login', path_names: { new: '/' }
   end
 
+  resources :users, only: :show, path: '/user' 
   resources :categories, exept: :show
   resources :products, path: '/'
 end
