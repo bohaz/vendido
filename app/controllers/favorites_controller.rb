92d6d129c247e423fa6e6351a225afbc55618a9e
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-22 16:13:18 -0400

    Add destroy method to favorites controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,6 +5,11 @@ class FavoritesController < ApplicationController
     redirect_to product_path(product)
   end 
 
+  def destroy
+    product.favorites.find_by(user: Current.user).destroy
+    redirect_to product_path(product), status: :see_other
+  end
+
   private
 
   def product
