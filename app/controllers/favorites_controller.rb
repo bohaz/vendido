1033724c9e0684746d600f69ffe3fb81d26113f6
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-22 16:30:29 -0400

    Refactor favorites controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,12 +1,12 @@
 class FavoritesController < ApplicationController
 
   def create
-   Favorite.create(product: product, user: Current.user)
+    product.favorite!
     redirect_to product_path(product)
   end 
 
   def destroy
-    product.favorites.find_by(user: Current.user).destroy
+    product.unfavorite!
     redirect_to product_path(product), status: :see_other
   end
 
