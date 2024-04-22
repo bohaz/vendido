28a5bfcddac25446573582de2dab807e0dce9545
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-22 16:30:55 -0400

    Include concerns

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
   include PgSearch::Model
+  include Favoritable
+
   pg_search_scope :search_full_text, against: {
     title: "A",
     description: "B"
@@ -15,7 +17,6 @@ class Product < ApplicationRecord
   validates :title, :description, :price, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
   
-  has_many :favorites, dependent: :destroy
   belongs_to :category
   belongs_to :user, default: -> { Current.user }
 
