91783fd895486a9769455cfa18d4a17198f46fa7
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-22 10:27:58 -0400

    Add finding products

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by!(username: params[:username])
+
+    @pagy, @products = pagy_countless(FindProducts.new.call({ user_id: @user.id }).load_async, items: 12)
   end
 end
