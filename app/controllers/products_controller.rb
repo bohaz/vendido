8fe18fd37f2f83b9d2bfaa952bd358623facdd41
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-23 08:50:05 -0400

    Add favorites param

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -53,6 +53,6 @@ class ProductsController < ApplicationController
   end
 
   def product_params_index
-    params.permit(:category_id, :min_price, :max_price, :query_text, :order_by, :page)
+    params.permit(:category_id, :min_price, :max_price, :query_text, :order_by, :page, :favorites)
   end
 end  
