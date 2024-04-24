bc6824465065b40472fb50d724c391d01b337a4c
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-24 18:35:15 -0400

    Update import map config

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,8 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "@rails/actioncable", to: "actioncable.esm.js"
-pin "@rails/activestorage", to: "activestorage.esm.js"
-pin "@rails/actiontext", to: "actiontext.esm.js"
-pin "trix"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/turbo-rails", to: "turbo.js" # @7.0.0
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "tailwindcss-stimulus-components" # @5.1.1
