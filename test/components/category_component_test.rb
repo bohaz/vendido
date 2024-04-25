684cbf18577702361eabf50fa5a0a3daa6bc8c18
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-25 12:09:47 -0400

    Render category component into index product view

---
+++ test/components/category_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class CategoryComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(CategoryComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
