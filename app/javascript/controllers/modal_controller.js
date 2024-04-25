eb2047768afd71f02ab6748a59aebbfb12c867d4
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-25 08:40:24 -0400

    Add the closing method

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,7 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["modal"]
   connect() {
  
+  }
+
+  close() {
+    this.modalTarget.classList.remove("translate-x-0")
+ console.log("close")
   }
 }
