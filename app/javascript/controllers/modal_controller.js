fb58cf4b2e4a961785a7b827f940727174c6e732
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-25 09:17:00 -0400

    Add a sidebar animation

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -3,7 +3,11 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["sidebar"]
   connect() {
- 
+    setTimeout(() => {
+      this.sidebarTarget.classList.remove("translate-x-full")
+      this.sidebarTarget.classList.add("translate-x-0")
+    }
+    , 50)
   }
 
   close() {
