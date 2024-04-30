142b4f55b5ad750453ef6aee380f201c71ea05b8
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-30 14:03:48 -0400

    Update job

--- app/jobs/fetch_country_job.rb
+++ app/jobs/fetch_country_job.rb
@@ -1,7 +1,11 @@
 class FetchCountryJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(user_id, ip)
+    country = FetchCountryService.new(ip).perform
+    user = User.find(user_id)
+    if country
+      user.update(country: country)
+    end
   end
 end
