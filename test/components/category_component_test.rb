aa0001174898ac211044501285f9b843d91cd1d8
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-30 08:13:23 -0400

    Add category component test

--- test/components/category_component_test.rb
+++ test/components/category_component_test.rb
@@ -4,9 +4,9 @@ require "test_helper"
 
 class CategoryComponentTest < ViewComponent::TestCase
   def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(CategoryComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+    assert_equal(
+     %(<a class=\"category text-gray-600 px-4 py-2 rounded-2xl drop-shadow-sm hover:bg-gray-300 bg-gray-300\" href=\"/\">Todos</a>),
+     render_inline(CategoryComponent.new).to_html
+    )
   end
 end
