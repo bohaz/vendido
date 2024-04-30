7ceeaf4680246d291c14d35fe06f2ee2a520e301
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-30 12:05:00 -0400

    Add fetch country dervice

--- app/controllers/authentication/users_controller.rb
+++ app/controllers/authentication/users_controller.rb
@@ -7,7 +7,7 @@ class Authentication::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
+    @user.country = FetchCountryService.new("24.48.0.1").perform
     if @user.save
       UserMailer.with(user: @user).welcome.deliver_later
       session[:user_id] = @user.id
