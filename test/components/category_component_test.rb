13e8ec92c50dd23e8e5a9783b7a791bbe997d7e1
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-30 08:17:09 -0400

    Refactor test

--- test/components/category_component_test.rb
+++ test/components/category_component_test.rb
@@ -3,7 +3,7 @@
 require "test_helper"
 
 class CategoryComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
+  test "should render an empty category" do
     assert_equal(
      %(<a class=\"category text-gray-600 px-4 py-2 rounded-2xl drop-shadow-sm hover:bg-gray-300 bg-gray-300\" href=\"/\">Todos</a>),
      render_inline(CategoryComponent.new).to_html
