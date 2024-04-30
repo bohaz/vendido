4e70e47e6b5ab691f7ae8463fb2ee1c783f26b29
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-30 09:23:49 -0400

    Add new test

--- test/components/category_component_test.rb
+++ test/components/category_component_test.rb
@@ -9,4 +9,13 @@ class CategoryComponentTest < ViewComponent::TestCase
      render_inline(CategoryComponent.new).to_html
     )
   end
+
+  test "should render category" do
+    category = Category.last
+    assert_equal(
+     %(<a class=\"category text-gray-600 px-4 py-2 rounded-2xl drop-shadow-sm hover:bg-gray-300 bg-white\" href=\"/?category_id=#{category.id}\">#{category.name}</a>),
+     render_inline(CategoryComponent.new(category: Category.last)).to_html
+    )
+  end
+
 end
