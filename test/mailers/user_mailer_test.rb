55066e0acf2f2116738c8671bfbb270c6feb32e8
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-30 11:02:38 -0400

    Update test

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,12 +1,15 @@
 require "test_helper"
 
 class UserMailerTest < ActionMailer::TestCase
-  test "welcome" do
-    mail = UserMailer.welcome
-    assert_equal "Welcome", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+  def setup
+    @user = User.last
   end
 
+  test "welcome" do
+    mail = UserMailer.with(user: @user).welcome
+    assert_equal "Bienvenido a Vendido", mail.subject
+    assert_equal [@user.email], mail.to
+    assert_equal ["no-reply@vendido.com"], mail.from
+    assert_match "Hey #{@user.username}, bienvenid@ a Vendido.", mail.body.encoded
+  end
 end
